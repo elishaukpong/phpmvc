6989035f0a7c76d2bbb6a3114cbc4cf17ef1ecb5
drumzminister <ishukpong418@gmail.com>
2019-04-27 14:42:01 +0100

    made config file and abstrace database connection details

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'mvc',
+        'username' => 'root',
+        'password' => '',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => []
+    ]
+];
--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-require 'Task.php';
-require 'function.php';
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
+$query = require 'bootstrap.php';
 
 
-$tasks = (new QueryBuilder(Connection::make()))->selectAll('task');
+$tasks = $query->selectAll('task');
 
 require 'index.view.php';
