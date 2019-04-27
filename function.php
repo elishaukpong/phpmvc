893c16b816c4e8bd55f18942b4e3acbb36c86186
drumzminister <ishukpong418@gmail.com>
2019-04-27 14:01:17 +0100

    abstracted query builder to class

--- function.php
+++ function.php
@@ -5,19 +5,3 @@ function dd($data){
     die(var_dump($data));
     echo "</pre>";
 }
-
-function connectToDb(){
-    try{
-        return new PDO('mysql:host=127.0.0.1;dbname=mvc', 'root', '');
-    }catch(PDOException $e){
-        die('Could not connect!');
-    }
-}
-
-function fetchAllTasks( $pdo )
-{
-    $statement = $pdo->prepare('select * from task');
-    $statement->execute();
-    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-
-}
--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 require 'Task.php';
 require 'function.php';
+require 'database/Connection.php';
+require 'database/QueryBuilder.php';
 
 
-$pdo = connectToDb();
-
-$tasks = fetchAllTasks($pdo);
+$tasks = (new QueryBuilder(Connection::make()))->selectAll('task');
 
 require 'index.view.php';
